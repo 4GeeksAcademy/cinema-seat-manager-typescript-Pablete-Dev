5372cb232452c211067d056fadb248e2ddacc0fa
Phbarrio <phbarrio@gmail.com>
2026-09-11 22:28:40 +0000

    feat: display cinema seat availability

--- src/main.ts
+++ src/main.ts
@@ -13,3 +13,26 @@ export function inicializarSala(): number[][] {
 
   return sala;
 }
+
+export function mostrarSala(sala: number[][]): void {
+  let encabezado = "   ";
+
+  for (let columna = 0; columna < 10; columna++) {
+    encabezado += `${columna + 1} `;
+  }
+
+  console.log(encabezado);
+
+  for (let fila = 0; fila < sala.length; fila++) {
+    let filaMostrada = `${fila + 1}  `;
+
+    for (let columna = 0; columna < sala[fila].length; columna++) {
+      filaMostrada += `${sala[fila][columna] === 0 ? "L" : "X"} `;
+    }
+
+    console.log(filaMostrada);
+  }
+}
+
+const sala = inicializarSala();
+mostrarSala(sala);
